35cc4a2cd3cd8d45dae3d115efd011275e7a304a
bit-paper <bitpaper8@proton.me>
2023-09-20 18:01:29 +0200

    add error handler on mysqli connect

--- app/install/steps/3.php
+++ app/install/steps/3.php
@@ -4,16 +4,30 @@ class Step
 {
 	protected int $ActualStep = 3;
 	protected $template;
+	protected string $error = '';
 
 	public function __construct()
 	{
 		$this->Do();
 	}
 
+	protected function CheckError()
+	{
+		if(isset($_GET['error']))
+		{
+			switch ($_GET['error'])
+			{
+				case '1':
+					$this->error = isset($_GET['msg']) ? $_GET['msg'] : 'Unknown error';
+					break;
+			}
+		}
+	}
 
-	// TODO: error handling for mysql data. is valid connection?
 	protected function Do()
 	{
+		$this->CheckError();
+
 		$headTemplate = new Template("./app/install/templates/head.html");
 		$headTemplate->AddEntry("{step}", $this->ActualStep);
 		$headTemplate->Replace();
@@ -22,8 +36,13 @@ class Step
 		$footerTemplate->AddEntry("{year}", date("Y"));
 		$footerTemplate->Replace();
 
+		$errorTemplate = new Template("./app/install/templates/other/error.html");
+		$errorTemplate->AddEntry("{error}", $this->error);
+		$errorTemplate->Replace();
+
 		$this->template = new Template("./app/install/templates/3.html");
 		$this->template->AddEntry("{head}", $headTemplate->templ);
+		$this->template->AddEntry("{error}", $errorTemplate->templ);
 		$this->template->AddEntry("{footer}", $footerTemplate->templ);
 		$this->template->Render(true);
 	}
