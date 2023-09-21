84c59ecb38c88f4be95f66255387782ab1bb8747
bit-paper <bitpaper8@proton.me>
2023-09-21 11:22:54 +0200

    add thread view (still todo)

--- app/forum/pages/thread.php
+++ app/forum/pages/thread.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once './app/helpers/avatar.php';
+require_once './app/helpers/username.php';
+require_once './app/helpers/time.php';
+
 class ThreadPage
 {
     private $template;
@@ -64,15 +68,42 @@ class ThreadPage
 
     private function fetchThread()
     {
-        $thread = $this->database->Query('SELECT t.*, u.username AS user_created, r.id AS rank_id, r.rank_name, r.rank_format
+        $thread = $this->database->Query('SELECT t.*, u.username AS user_name, u.avatar AS user_avatar, u.reputation AS user_reputation, u.last_active AS user_lastactive, r.id AS rank_id, r.rank_name, r.rank_format
                                     FROM bit_threads t
                                     JOIN bit_accounts u ON t.user_id = u.id
                                     JOIN bit_ranks r ON u.rank_id = r.id
                                     WHERE t.id = ?', $this->threadID)->FetchArray();
 
-        print_r($thread);
+        // i guess we need one query of that, or just add another label for the user in database.
+        $threadCount = $this->database->Query('SELECT COUNT(*) AS thread_count FROM bit_threads WHERE user_id = ?', $thread['user_id'])->FetchArray();
+        $postCount = $this->database->Query('SELECT COUNT(*) AS post_count FROM bit_posts WHERE user_id = ?', $thread['user_id'])->FetchArray();
+        $userPostCount = $threadCount['thread_count'] + $postCount['post_count'];
 
-        //$threadContentTemplate = new Template('./themes/' . $this->theme . '/templates/thread/thread_content.html');
+        Log::Log($thread);
+
+        $userStatsTemplate = new Template('./themes/' . $this->theme . '/templates/thread/thread_user_stats.html');
+        $userStatsTemplate->AddEntry('{user_reputation}', $thread['user_reputation']);
+        $userStatsTemplate->AddEntry('{user_postcount}', $userPostCount);
+        $userStatsTemplate->Replace();
+
+        $threadContentTemplate = new Template('./themes/' . $this->theme . '/templates/thread/thread_content.html');
+        $threadContentTemplate->AddEntry('{thread_title}', $thread['thread_title']);
+        $threadContentTemplate->AddEntry('{thread_timestamp}', RelativeTime::Format($thread['thread_timestamp']));
+        $threadContentTemplate->AddEntry('{thread_content}', $thread['thread_content']);
+        $threadContentTemplate->AddEntry('{thread_likes}', $thread['thread_likes'] > 0 ? $thread['thread_likes'] : '');
+        // TODO: Add like button or new like widgets for posts if user is logged!
+
+        $threadContentTemplate->AddEntry('{user_id}', $thread['user_id']);
+        $threadContentTemplate->AddEntry('{user_name}', Username::Format($thread['rank_format'], $thread['user_name']));
+        $threadContentTemplate->AddEntry('{user_avatar}', Avatar::GetPath($this->theme, $thread['user_avatar']));
+        $threadContentTemplate->AddEntry('{user_avatar_alt}', $thread['user_name']);
+        $threadContentTemplate->AddEntry('{user_rank}', $thread['rank_name']);
+        $threadContentTemplate->AddEntry('{user_stats}', $userStatsTemplate->templ);
+        $threadContentTemplate->Replace();
+
+        $this->thread = $threadContentTemplate->templ;
+
+        $this->forumDesc .= $thread['thread_title'];
     }
 
     private function fetchPosts()
@@ -85,7 +116,7 @@ class ThreadPage
         $this->fetchThread();
         $this->fetchPosts();
 
-        $stylesTemplate = new StylesWidget($this->theme, '/templates/forum/styles.html');
+        $stylesTemplate = new StylesWidget($this->theme, '/templates/thread/styles.html');
 
         $headTemplate = new HeadWidget($this->theme, $this->forumName, $this->forumDesc, $stylesTemplate->Template->templ);
 
