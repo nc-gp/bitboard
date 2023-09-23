81fb974f3335adbf66a56c1afe5985eb02cd3072
bit-paper <bitpaper8@proton.me>
2023-09-23 23:25:59 +0200

    fix

--- app/install/Setup.php
+++ app/install/Setup.php
@@ -254,7 +254,7 @@ class Setup
 					return;
 				}
 
-				require_once './app/database/config.php';
+				require_once './app/config.php';
 				$NewUserDatabase = new Database($config['host'], $config['user'], $config['pass'], $config['name']);
 
 				AccountController::Create(
@@ -287,7 +287,7 @@ class Setup
 					return;
 				}
 
-				require_once './app/database/config.php';
+				require_once './app/config.php';
 				$db = new Database($config['host'], $config['user'], $config['pass'], $config['name']);
 
 				$db->Query('UPDATE bit_settings SET forum_name = ? WHERE id = ?', $forumName, 0);
