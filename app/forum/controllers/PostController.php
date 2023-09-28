6c4eccbf93455c461880a73fe76a86c89b617c2b
bit-paper <bitpaper8@proton.me>
2023-09-28 13:56:19 +0200

    remmoved old code

--- app/forum/controllers/PostController.php
+++ app/forum/controllers/PostController.php
@@ -4,35 +4,6 @@ namespace App\Forum\Controllers;
 
 class PostController
 {
-    public array $Data;
-    private int $ID;
-
-    public function __construct($db, int $id)
-    {
-        $this->ID = $id;
-        $this->Data = $db->Query('SELECT * FROM bit_posts WHERE id = ? LIMIT 1', $this->ID)->FetchArray();
-    }
-
-    public function UpdateAuthor($db, int $newAuthorID)
-    {
-        $db->Query('UPDATE bit_posts SET user_id = ? WHERE id = ?', $newAuthorID, $this->ID);
-    }
-
-    public function UpdateThreadID($db, int $newThreadID)
-    {
-        $db->Query('UPDATE bit_posts SET thread_id = ? WHERE id = ?', $newThreadID, $this->ID);
-    }
-
-    public function UpdateContent($db, string $newPostContent)
-    {
-        $db->Query('UPDATE bit_posts SET post_content = ? WHERE id = ?', $newPostContent, $this->ID);
-    }
-
-    public function UpdateLikes($db, bool $addOrRemove = false)
-    {
-        $db->Query('UPDATE bit_posts SET post_likes = ? WHERE id = ?', $addOrRemove ? $this->Data['post_likes'] + 1 : $this->Data['post_likes'] - 1, $this->ID);
-    }
-
     static public function Create($db, int $authorID, int $threadID, string $postContent)
     {
         $postTime = date("Y-m-d H:i:s");
