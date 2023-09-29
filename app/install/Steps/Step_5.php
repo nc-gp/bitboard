4f850e00d3dca65f733e0c35c0651b24f3c94fe6
bit-paper <bitpaper8@proton.me>
2023-09-29 19:48:15 +0200

    changed two queries into one

--- app/install/Steps/Step_5.php
+++ app/install/Steps/Step_5.php
@@ -67,8 +67,7 @@ class Step_5 extends StepBase implements StepInterface
 		require_once './app/config.php';
 		$db = new Database($config['host'], $config['user'], $config['pass'], $config['name']);
 
-		$db->Query('UPDATE bit_settings SET forum_name = ? WHERE id = ?', "$forumName", 0);
-		$db->Query('UPDATE bit_settings SET forum_description = ? WHERE id = ?', "$forumDesc", 0);
+		$db->Query('UPDATE bit_settings SET forum_name = ?, forum_description = ? WHERE id = ?', "$forumName", "$forumDesc", 0);
 
 		$db->Close();
 
