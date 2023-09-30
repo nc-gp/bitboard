b75cbfbb8f145ccd2214e664d59def61c23ff428
bit-paper <bitpaper8@proton.me>
2023-09-30 13:25:09 +0200

    removed enters D:

--- app/forum/controllers/RankController.php
+++ app/forum/controllers/RankController.php
@@ -13,21 +13,12 @@ class RankController
 
     static public function CreateTable(Database $db)
     {
-        $db->Query('CREATE TABLE IF NOT EXISTS bit_ranks (
-			id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-			rank_name VARCHAR(64) NOT NULL,
-			rank_format VARCHAR(256) NOT NULL,
-			rank_flags INT(6) NOT NULL
-		)');
+        $db->Query('CREATE TABLE IF NOT EXISTS bit_ranks (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, rank_name VARCHAR(64) NOT NULL, rank_format VARCHAR(256) NOT NULL, rank_flags INT(6) NOT NULL)');
     }
 
     static public function Create(Database $db, string $rankName, string $rankFormat, int $rankFlags = 0000000000)
     {
-        $db->Query("INSERT INTO bit_ranks 
-            (id,rank_name,rank_format,rank_flags) 
-            VALUES (?,?,?,?)", 
-            [0, $rankName, $rankFormat, $rankFlags]
-        );
+        $db->Query("INSERT INTO bit_ranks (id,rank_name,rank_format,rank_flags) VALUES (?,?,?,?)", [0, $rankName, $rankFormat, $rankFlags]);
     }
 }
 
